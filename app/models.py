735b6278c87083af10c501c44f08e656e98f637e
Ngina <Nginagitau11@outlook.com>
2022-05-16 16:48:57 +0300

    update:adds user models

--- app/models.py
+++ app/models.py
@@ -2,3 +2,34 @@ from . import db
 from . import login_manager
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash,check_password_hash
+
+class User(UserMixin,db.Model):
+    """
+    User class that will help in creating users
+    """
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(255))
+    email = db.Column(db.String(255),unique = True,index = True)
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String(255))
+    pass_secure = db.Column(db.String(255))
+    password_hash = db.Column(db.String(255))
+    photos = db.relationship('PhotoProfile',backref = 'user',lazy = "dynamic")
+    pitches = db.relationship('Pitch',backref = 'user',lazy = "dynamic")
+    comments = db.relationship('Comment',backref = 'user',lazy = "dynamic")
+    vote = db.relationship('Vote',backref = 'user', lazy = "dynamic")
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.pass_secure, password)
+
+    def __repr__(self):
+        return f'User {self.username}'

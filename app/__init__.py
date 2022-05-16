eb94321620cc68a254b08b2282fe5edb22fcc74e
Ngina <Nginagitau11@outlook.com>
2022-05-16 15:22:35 +0300

    update: adds flask initializations

---
+++ app/__init__.py
@@ -0,0 +1,52 @@
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_uploads import UploadSet, configure_uploads, IMAGES
+from flask_mail import Mail
+from flask_wtf.csrf import CSRFProtect, CSRFError
+from config import config_options
+
+#Initializing Application and Initializing Flask Extensions
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+
+
+bootstrap = Bootstrap()
+db = SQLAlchemy()
+photos = UploadSet('photos', IMAGES)
+mail = Mail()
+csrf = CSRFProtect()
+
+def create_app(config_name):
+    """Creating app configurations
+        Initializing flask extensions
+        Registering the blueprint
+        setting Config
+        Adds the views and forms
+    """
+
+    app = Flask(__name__)
+
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
+    config_options[config_name].init_app(app)
+
+
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+    db.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
+    csrf.init_app(app)
+
+    # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+
+
+
+
+    return app

7e19ffa977da8fd584e2356a542e5f895d52be6c
Ngina <Nginagitau11@outlook.com>
2022-05-16 17:00:19 +0300

    update: branch Ngina adds working user model and tests

--- app/models.py
+++ app/models.py
@@ -9,8 +9,32 @@ class User(UserMixin,db.Model):
     """
     __tablename__ = 'users'
 
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(255))
+    email = db.Column(db.String(255),unique = True,index = True)
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String(255))
+    pass_secure = db.Column(db.String(255))
+    password_hash = db.Column(db.String(255))
+    photos = db.relationship('PhotoProfile',backref = 'user',lazy = "dynamic")
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.pass_secure, password)
+
+    def __repr__(self):
+        return f'User {self.username}'
 
 
 class PhotoProfile(db.Model):
     __tablename__ = 'profile_photos'
 
+    id = db.Column(db.Integer,primary_key = True)
+    pic_path = db.Column(db.String())
+    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))

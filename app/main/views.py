ede787ba314f0de7a0c3e259100a8b235bf91a41
Ngina <Nginagitau11@outlook.com>
2022-05-16 16:59:06 +0300

    update: adds flask migration

--- app/main/views.py
+++ app/main/views.py
@@ -1,3 +1,5 @@
 from flask import render_template, redirect,url_for, abort
 from flask_login import login_required, current_user
 from . import main
+from .. import db,photos
+from ..models import User, PhotoProfile
--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 from app import create_app,db
 from flask_script import Manager,Shell,Server
 from flask_migrate import Migrate, MigrateCommand
-from app.models import User
+from app.models import User, PhotoProfile
 
 # app = create_app('development')
 app = create_app('test')

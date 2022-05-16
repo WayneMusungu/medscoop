700e967c536da93e3bfa02965ec267097a91d23c
Ngina <Nginagitau11@outlook.com>
2022-05-16 16:49:31 +0300

    update:adds the database and migration

--- manage.py
+++ manage.py
@@ -1,6 +1,6 @@
 from app import create_app,db
 from flask_script import Manager,Shell,Server
-# from flask_migrate import Migrate, MigrateCommand
+from flask_migrate import Migrate, MigrateCommand
 from app.models import User
 
 # app = create_app('development')
@@ -10,8 +10,8 @@ app = create_app('production')
 manager = Manager(app)
 manager.add_command('server', Server)
 
-# migrate = Migrate(app,db)
-# manager.add_command('db',MigrateCommand)
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
 
 @manager.command
 def test():
